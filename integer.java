13f39c1849a0dea743a18468ab283ec725d5cf0c
vaibhav1491 <102009018+vaibhav1491@users.noreply.github.com>
2024-10-14 14:59:52 +0530

    Add files via upload

---
+++ integer.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Basic {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=5;
+        float b=8.98f;
+        double c=85.543;
+        long d=1000000000000L;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        
+    }
+}
